a0d993def18683a8e86804b24245611fa9b04db1
Cateiru <yuto.w51942@gmail.com>
2023-12-18 18:50:06 +0900

    エラーをちゃんとして、成功時に紙吹雪

--- app/components/ButtonPage.tsx
+++ app/components/ButtonPage.tsx
@@ -1,11 +1,29 @@
 "use client";
 
-import { Box, Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
 import { OfficeButton } from "./OfficeButton";
 import { useInOffice } from "./useInOffice";
+import { SuccessDialog } from "./SuccessDialog";
+import React from "react";
 
 export const ButtonPage = () => {
-  const { inOffice, loading, handleInOffice } = useInOffice();
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState<Error | null>(null);
+  const { inOffice, handleInOffice } = useInOffice();
+
+  const handleClick = () => {
+    setError(null);
+    setLoading(true);
+
+    handleInOffice()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((e) => {
+        setError(e);
+        setLoading(false);
+      });
+  };
 
   return (
     <Center w="100%" h="100vh">
@@ -19,10 +37,22 @@ export const ButtonPage = () => {
           出社できて偉いボタン
         </Heading>
         <Center>
-          <OfficeButton onClick={handleInOffice} isLoading={loading}>
+          <OfficeButton
+            onClick={handleClick}
+            isLoading={loading}
+            isError={error !== null}
+          >
             出社
           </OfficeButton>
         </Center>
+        {error ? (
+          <Text textAlign="center" my="1rem" fontWeight="bold" color="red.500">
+            {error.message}
+          </Text>
+        ) : (
+          <Box h="3rem"></Box>
+        )}
+        <SuccessDialog isSuccess={inOffice} />
       </Box>
     </Center>
   );
