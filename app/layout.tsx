f62a903ec0199e48deb63a656ee69d8b36810137
Cateiru <yuto.w51942@gmail.com>
2023-12-18 18:14:06 +0900

    og image設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <title>出社できて偉いボタン</title>
+
+        <meta property="og:title" content="出社できて偉いボタン" />
+        <meta property="og:site_name" content="出社できて偉いボタン" />
+        <meta property="og:url" content="https://office.cateiru.com/" />
+        <meta
+          property="og:image"
+          content="https://office.cateiru.com/ogp.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
